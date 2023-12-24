3c7e8b00161e7087c9b7bad4d34c876e6a0e31e0
dervis <dervishalilcevic@gmail.com>
2023-12-24 23:46:19 +0100

    Add TLS configuration for HTTPS; logger; dotenv files

--- server.go
+++ server.go
@@ -1,31 +1,80 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
-func Handle(w http.ResponseWriter, r *http.Request) { 
+type Logger struct {
+	LogFileName string
+	Location    string
+}
+
+func (e *Logger) Log(s string) {
+	f, err := os.OpenFile(e.Location+e.LogFileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		fmt.Println("Error Log() ")
+		fmt.Println(err)
+	}
+
+	f.WriteString("[ " + time.Now().Format("01-02-2001 15:04:05") + " ] - " + s + "\n")
+	fmt.Println(s)
+}
+
+func Handle(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, r.URL.Host, http.StatusUseProxy)
 }
 
-func main() {
-	fmt.Println("Server is starting... ")
+func loadCertificate(certFile, keyFile string) tls.Certificate {
+	cert, err := tls.LoadX509KeyPair(certFile, keyFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Certificates are loaded!")
+	return cert
+}
 
-	router := mux.NewRouter();
-	
+func main() {
+	l := Logger{
+		LogFileName: "proxy.log",
+		Location:    "/var/log/",
+	}
+
+	l.Log("Server is starting ...")
+
+	if err := godotenv.Load(); err != nil {
+		fmt.Println("Error loading dotenv files!")
+	}
+
+	CertificatePath := os.Getenv("CERTIFICATE_PATH")
+
+	router := mux.NewRouter()
+
+	tls := &tls.Config{
+		Certificates: []tls.Certificate{
+			loadCertificate(CertificatePath+"cert.pem", CertificatePath+"key.pem"),
+		},
+	}
+
 	router.HandleFunc("/", Handle)
 
 	s := http.Server{
-		Addr: ":8080",
-		Handler: router,
+		Addr:      ":8080",
+		Handler:   router,
+		TLSConfig: tls,
 	}
 
-	err := s.ListenAndServe();
+	err := s.ListenAndServe()
 	if err != nil {
-		fmt.Println("Error");
+		fmt.Println("Error ListenAndServe")
 	}
 
 }
